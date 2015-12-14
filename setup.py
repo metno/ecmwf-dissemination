38e5475532594a81cc6d1ace8c2580a9855f9652
Alexander Bürger <alexanderb@met.no>
2015-12-14 09:00:09 +0100

    update git repo url

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
     'description': 'ECDISS daemons',
     'author': 'IT-GEO-TF',
-    'url': 'https://gitlab.met.no/it-geo/ecmwf-dissemination',
-    'download_url': 'https://gitlab.met.no/it-geo/ecmwf-dissemination',
+    'url': 'https://github.com/metno/ecmwf-dissemination',
+    'download_url': 'https://github.com/metno/ecmwf-dissemination',
     'author_email': 'it-geo-tf@met.no',
     'version': '0.1',
     'install_requires': [
